93d28db85886bcc8e7422750ba34765e67fe7175
umesh <umesh@LAPTOP-1GAVEG1D>
2020-02-16 17:02:21 +0530

    my first  docker image

---
+++ docker-boot-intro/src/main/java/com/samples/docker/DockerBootIntroApplication.java
@@ -0,0 +1,14 @@
+package com.samples.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBootIntroApplication {
+
+	public static void main(String[] args) {
+		System.out.println("jai mahakal ---");
+		SpringApplication.run(DockerBootIntroApplication.class, args);
+	}
+
+}
